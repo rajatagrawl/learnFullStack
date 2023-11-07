e731c7408b887fa9f3d1957172165340d1c2dd62
rajatagrawl <rajat1611@gmail.com>
2023-11-07 06:08:32 +0530

    Add varaibles.js

---
+++ BasicsJS/variables.js
@@ -0,0 +1,20 @@
+let userName = "Rajat"
+const userID = 123
+
+console.log(userName)
+console.log(userID)
+
+
+/*
+Variable Types:
+1. Primitive
+a. String
+b. Integer
+c. undefined
+d. null
+e. Bigint
+
+2. Non-primitive
+
+
+*/
