8ae778db93c884d6046774252eacc9373cdca304
rajatagrawl <rajat1611@gmail.com>
2023-11-08 06:00:27 +0530

    Add operators.js

---
+++ BasicsJS/operators.js
@@ -0,0 +1,11 @@
+// Arithmetic Operator
+
+let numberOne = 2 + 2
+console.log(numberOne)
+
+// Assignment Operator
+let numberTwo = numberOne
+console.log(numberTwo)
+
+// Compsrison Operator
+console.log(numberOne === numberTwo)
