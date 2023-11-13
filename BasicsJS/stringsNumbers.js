b592acd129f0161adfa7aa1466ee93b20b3bb2fb
rajatagrawl <rajat1611@gmail.com>
2023-11-13 16:27:49 +0530

    Add stringsNumbers.js

---
+++ BasicsJS/stringsNumbers.js
@@ -0,0 +1,7 @@
+let firstName = "Rajat"
+let lastName = "Agrawal"
+
+console.log(`My First name is ${firstName} and last name is ${lastName}`)
+
+let word = "agra"
+console.log(lastName.includes(word, lastName))
