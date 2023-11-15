ab58c79fa0ecd9dd65ce3d75d98d3fee866cb31e
rajatagrawl <rajat1611@gmail.com>
2023-11-15 10:15:38 +0530

    Practice Numbers

--- BasicsJS/stringsNumbers.js
+++ BasicsJS/stringsNumbers.js
@@ -1,7 +1,15 @@
-let firstName = "Rajat"
-let lastName = "Agrawal"
+// let firstName = "Rajat"
+// let lastName = "Agrawal"
 
-console.log(`Hello, My First name is ${firstName} and last name is ${lastName}`)
+// console.log(`Hello, My First name is ${firstName} and last name is ${lastName}`)
 
-let word = "agra"
-console.log(lastName.includes(word, lastName))
+// let word = "agra"
+// console.log(lastName.includes(word, lastName))
+
+
+// let num = new Number(5)
+// num = 5.296845
+// console.log(num.toFixed(2))
+
+
+console.log(Math.floor(Math.random()*100));
