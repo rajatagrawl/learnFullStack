5345ba4fd0072961f84debd879cd76dd3665aea4
rajatagrawl <rajat1611@gmail.com>
2023-11-15 10:34:35 +0530

    Add myArray.js

---
+++ BasicsJS/myArray.js
@@ -0,0 +1,24 @@
+// const myArray = [1, 2, "Rajat", "Rohit"]
+// console.log(myArray);
+
+// const myNewArray = new Array(7, 8, "Yuvraj", "Sachin")
+// console.log(myNewArray);
+
+const marvel = ["Ironman", "Captain America", "Hulk", "Natasha", "Spiderman"]
+// console.log(marvel);
+// marvel.push("Thor")
+// console.log(marvel);
+// const poppedOne = marvel.pop()
+// console.log(marvel);
+// console.log(poppedOne);
+
+// console.log(marvel.unshift("Natasha"))
+// console.log(marvel);
+
+// console.log(marvel.toString());
+// console.log(marvel.join("|"));
+
+console.log(marvel.slice(1,3));
+console.log(marvel);
+console.log(marvel.splice(1,3));
+console.log(marvel);
