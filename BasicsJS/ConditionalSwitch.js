789ca06414f02301268c805d0261537ba1bd4bde
Rajat Agrawal <rajat.agarwal@walmart.com>
2023-11-28 14:59:40 +0530

    Add SwitchCase

--- BasicsJS/ConditionalSwitch.js
+++ BasicsJS/ConditionalSwitch.js
@@ -1,1 +1,24 @@
-let reviewRating = "5"
+let reviewRating = "7"
+
+revInInt = parseInt(reviewRating);
+
+switch (revInInt) {
+    case 1:
+        console.log("Poor");
+        break;
+    case 2:
+        console.log("Below expectations");
+        break;
+    case 3:
+        console.log("OK");
+        break;
+    case 4:
+        console.log("Hmm Goood");
+        break;
+    case 5:
+        console.log("Superrrr");
+        break;
+    default:
+        console.log("Please ebter rating again");
+        break;
+}
