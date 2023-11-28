9de2b5f8ac75d21f112138da1179f2dbbe5c45b0
Rajat Agrawal <rajat.agarwal@walmart.com>
2023-11-28 14:46:13 +0530

    Conditional if

---
+++ BasicsJS/ConditionalIf.js
@@ -0,0 +1,19 @@
+let number = 100;
+
+if (number === 100) {
+    console.log("Value matched");
+} else {
+    console.log("Value did not match");
+}
+
+/*
+Falsy values
+false
+0
+""
+null
+undefined
+NaN
+*/
+
+(number === 100) ? console.log("Value matched") : console.log("Value did not match");
